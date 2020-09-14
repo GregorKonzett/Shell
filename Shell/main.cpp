d58461b4e7df07cc4daf7f447cadfe6445ba6f1f
Reason1338 <gregor.konzett98@gmail.com>
2020-09-14 10:15:51 +0100

    Initial Commit

---
+++ Shell/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Shell
+//
+//  Created by Gregor Konzett on 14.09.20.
+//  Copyright © 2020 Gregor Konzett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
